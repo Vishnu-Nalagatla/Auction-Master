6a854c3dca2bee520b4db3406f41efb77268ca97
Vishnu Nalagatla <vishnuvardhanreddy.n@techouts.com>
2023-01-16 16:01:48 +0530

    app file pretier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router,Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from "./components/AuctionHomePage/Header/Navbar/Navbar";
 import AuctionHomePage from "./components/AuctionHomePage/AuctionHomePage";
 import ProductsServices from "./components/AuctionProductsServices/ProductsServices";
@@ -11,28 +11,28 @@ import SignUpYoutube from "./components/AuctionHomePage/Header/SignUp-Page/SignU
 function App() {
   return (
     <div className="App">
-      {/* <div className="nav-container"><Navbar></Navbar></div>
-
-      
-        <Switch>
-          <Route exact path='/'>
-      <AuctionHomePage></AuctionHomePage>
-
-      </Route>
-      <Route exact path='/products&services'>
-      <ProductsServices></ProductsServices>
-      </Route>
-<Route path="/signup" exact component={SignUp} ></Route>
-
-<Route path="/LoginPage" exact >
-  <UserForm></UserForm>
-</Route>
-        </Switch> */}
+      <div className="nav-container"><Navbar></Navbar></div>
 
 
-<SignUpYoutube></SignUpYoutube>
+      <Switch>
+        <Route exact path='/'>
+          <AuctionHomePage></AuctionHomePage>
 
-      
+        </Route>
+        <Route exact path='/products&services'>
+          <ProductsServices></ProductsServices>
+        </Route>
+        <Route path="/signup" exact component={SignUp} ></Route>
+
+        <Route path="/LoginPage" exact >
+          <UserForm></UserForm>
+        </Route>
+      </Switch>
+
+
+      {/* <SignUpYoutube></SignUpYoutube> */}
+
+
 
 
     </div>
